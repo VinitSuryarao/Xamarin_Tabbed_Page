6b6519631a5b0437c7243fb70554f6f146061caa
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-07-02 17:50:04 +0530

    First Commit

---
+++ Tabbed_Page/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tabbed_Page
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new MainPage(), new Page1(), new Page2()
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
